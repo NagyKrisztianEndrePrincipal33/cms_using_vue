fc939b408f2472b61a0564c6a7d9bb7aeb9c9565
AzureAD\KrisztianNagy <krisztian.nagy@principal33.com>
2021-08-19 21:22:21 +0300

    Addition: Creating Header for the page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
+  <Header headerText="Content management system"></Header>
   <Table></Table>
-  
 </template>
 
 <script>
 import Table from './components/Table/Table.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    Table
+    Table,
+    Header,
   },
   data(){
     return{
@@ -29,6 +31,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <h1>{{ headerText }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    headerText: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Style+Script&display=swap");
+.header {
+  padding: 25px;
+  box-sizing: border-box;
+  background-color: rgb(17, 42, 121);
+  margin-bottom: 15px;
+  position: sticky;
+  box-shadow: 1px 2px 20px rgba(0, 0, 0, 0.5);
+  top: 0;
+  h1 {
+    color: white;
+    font-family: "Style Script", cursive;
+    font-size: 4rem;
+  }
+}
+</style>
